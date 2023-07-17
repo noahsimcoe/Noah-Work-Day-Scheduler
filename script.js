07115b4903c1f69c51a25a76253cf9927b60a106
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-07-17 11:24:27 -0500

    refactored add/remove class function to use jquery, and simplified the html by using ID instead of a data-value property

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 $(function () {
-  // current hour is saved
+  // current hour is saved as a number
   var currentHour = Number(dayjs().format('H'));
-  // removes/adds class to hour block if it's in past/present/future
-  var timeBlocks = document.getElementsByClassName("time-block");
-  for (var i = 0; i < timeBlocks.length; i++) {
-    var hourValue = parseInt(timeBlocks[i].getAttribute("data-value"), 10);
+
+  /* loops through each of the .time-block divs and adds/removes a class
+  whether or not their id matches the current hour */
+  $('.time-block').each(function() {
+    // converts the id from a string to a number
+    var hourValue = Number($(this).attr("id"));
+
     if (currentHour > hourValue) {
-      timeBlocks[i].classList.remove("present");
-      timeBlocks[i].classList.remove("future");
-      timeBlocks[i].classList.add("past");
+      // this is refering to the current .time-block div that it's looped on
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
     } else if (currentHour < hourValue) {
-      timeBlocks[i].classList.remove("past");
-      timeBlocks[i].classList.remove("present");
-      timeBlocks[i].classList.add("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
     } else {
-      timeBlocks[i].classList.remove("past");
-      timeBlocks[i].classList.remove("future");
-      timeBlocks[i].classList.add("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
     }
-  }
+  });
+
   // TODO: Add code to display the current date in the header of the page.
   var currentTime = dayjs().format('dddd, MMM D');
   $('#display-4').text(currentTime);
@@ -30,7 +35,7 @@ $(function () {
   function saveToLocalStorage() {
     // finds the save button's parents's id
     var blockId = $(this).parent().attr("id");
-    // finds the save button's sibling's (.description) value (this user inputted area)
+    // finds the save button's siblings's (.description) value (the user inputted area)
     var blockDescription = $(this).siblings(".description").val();
     // saves description according to the id of it's container
     localStorage.setItem(blockId, blockDescription);
@@ -45,5 +50,4 @@ $(function () {
   $("#14 .description").val(localStorage.getItem("14"));;
   $("#15 .description").val(localStorage.getItem("15"));;
   $("#16 .description").val(localStorage.getItem("16"));;
-
 });
